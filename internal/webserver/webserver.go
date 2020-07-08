f3b60da79ff7696efbf37051cdc206571da568e8
Steffen Park <dev@istherepie.com>
2020-07-08 18:36:00 +0100

    Add handler for multipart file upload

--- internal/webserver/webserver.go
+++ internal/webserver/webserver.go
@@ -3,7 +3,9 @@ package webserver
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 type JSONResponse struct {
@@ -23,7 +25,7 @@ func (j *JSONResponse) Response(w http.ResponseWriter) {
 
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 
@@ -33,9 +35,50 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("ALLOW CORS")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	r.ParseMultipartForm(15000000)
+
+	uploadFile, handler, err := r.FormFile("uploadfile")
+	defer uploadFile.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Respond to client
+	j := &JSONResponse{200, "Upload completed"}
+	j.Response(w)
+
+	// Store file locally
+	// TODO: Store in the /tmp directory, however this should be configurable
+	fileName := fmt.Sprintf("/tmp/%s", handler.Filename)
+
+	localFile, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE, 0666)
+	defer localFile.Close()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Copy uploaded file
+	io.Copy(localFile, uploadFile)
+
+}
+
 func New() *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", index)
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/upload", uploadHandler)
 
 	return mux
 }
